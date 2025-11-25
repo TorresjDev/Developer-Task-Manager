cac053ef9022f2f6830233592ca51dd63858744a
TorresjDev <j.torres3.dev@gmail.com>
2025-11-24 19:59:11 -0600

    feat: Initialize ASP.NET Core Razor pages app with EF Core and SQLite

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Developer_Task_Manager.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
